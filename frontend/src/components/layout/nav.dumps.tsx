3a1798288d9de237c00acecb01778ddf81e14bce
goodnews <goodnewsj62@gmail.com>
2024-09-05 10:02:45 +0100

    bug fix

---
+++ frontend/src/components/layout/nav.dumps.tsx
@@ -0,0 +1,60 @@
+{
+  /* <nav className="hidden items-center gap-8 lg:flex">
+<NavLink
+  to={routes.ABOUT}
+  className={({ isActive }) =>
+    isActive ? `${DEFAULT_NAV_STYLES}` : `${DEFAULT_NAV_STYLES}`
+  }
+>
+  About
+</NavLink>
+{!is_authenticated && (
+  <>
+    <Link to={routes.LOGIN}>
+      <Button
+        variant="contained"
+        sx={{
+          textTransform: "capitalize",
+          fontWeight: "600",
+          paddingRight: "2rem",
+          paddingLeft: "2rem",
+        }}
+        disableElevation
+      >
+        Login
+      </Button>
+    </Link>
+    <Link to={routes.REGISTER_PAGE}>
+      <Button
+        variant="contained"
+        sx={{
+          textTransform: "capitalize",
+          fontWeight: "600",
+          paddingRight: "2rem",
+          paddingLeft: "2rem",
+        }}
+        disableElevation
+      >
+        Register
+      </Button>
+    </Link>
+  </>
+)}
+
+{is_authenticated && (
+  <Button
+    variant="contained"
+    sx={{
+      textTransform: "capitalize",
+      fontWeight: "600",
+      paddingRight: "2rem",
+      paddingLeft: "2rem",
+    }}
+    disableElevation
+    onClick={logout}
+  >
+    Logout
+  </Button>
+)}
+</nav> */
+}

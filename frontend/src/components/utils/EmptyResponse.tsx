337ee36e371bc8559d9d72d4ee0c73327155b31e
goodnews <goodnewsj62@gmail.com>
2024-09-05 01:13:38 +0100

    homepage

--- frontend/src/components/utils/EmptyResponse.tsx
+++ frontend/src/components/utils/EmptyResponse.tsx
@@ -1,20 +1,21 @@
-
-
 import React from "react";
 
-interface Props{
-    message: string,
-    src:string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    styles?: Record<string, any>
+interface Props {
+  message: string;
+  src: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  styles?: Record<string, any>;
 }
-const EmptyResponse:React.FC<Props> = ({message,  src, styles={}}) => {
-    return (
-        <div className="w-full flex flex-col items-center justify-center" style={{height:"calc(100vh - 100px)", ...styles}}>
-            <img src={src} className="w-32" alt="image description" />
-            <p className="mt-4 text-center px-4">{message}</p>
-        </div>
-    );
-}
+const EmptyResponse: React.FC<Props> = ({ message, src, styles = {} }) => {
+  return (
+    <div
+      className="flex w-full flex-col items-center justify-center"
+      style={{ height: "calc(100vh - 100px)", ...styles }}
+    >
+      <img src={src} className="w-32" alt="image description" />
+      <p className="mt-4 px-4 text-center">{message}</p>
+    </div>
+  );
+};
 
 export default EmptyResponse;

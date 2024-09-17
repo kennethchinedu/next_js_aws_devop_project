4f71e40d36dc58fac062cfcf9076cf011a5f31af
Kenneth Chinedu <67561307+kennethchinedu@users.noreply.github.com>
2024-09-17 01:55:32 +0100

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,14 @@
+#We will be storing our statefile in aws s3 with dynamo db for statelock
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-file-anams"
+    #dynamodb_table = "lock_files"
+    key = "global/mystatefile/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 resource "aws_s3_bucket" "mybucket" {
   bucket = "terraform-state-file-anams"
   versioning {

b0402015812530aa81a518107348ee97f3845609
Kenneth Chinedu <67561307+kennethchinedu@users.noreply.github.com>
2024-09-17 01:46:34 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-file-anams"
-    dynamodb_table = "lock_files"
+    #dynamodb_table = "lock_files"
     key = "global/mystatefile/terraform.tfstate"
     region = "us-east-1"
   }

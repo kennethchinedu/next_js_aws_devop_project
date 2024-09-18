6546d30799c181f3a33d689e1b4695cc6fe7e8df
kennethchinedu <anamskenneth@gmail.com>
2024-09-18 02:04:36 +0100

    changes 146

--- infra/main.tf
+++ infra/main.tf
@@ -3,8 +3,39 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "region_main" {
+  type        = string
+  # default = "us-east-1"
+}
+
+variable "cidr" {
+  type        = string
+  # default = "172.16.0.0/16"
+}
+
+variable "availability_zone_a" {
+  type        = string
+  # default = "us-east-1a" 
+}
+
+variable "availability_zone_b" {
+  type        = string
+  # default = "us-east-1b" 
+}
+
+variable "ami" {
+  type        = string
+  #  description = "ami-0a0e5d9c7acc336f1"
+}
+
+variable "instance_type" {
+  type        = string
+  # default = "t2.micro"
+}
+
+
 module "app-deployment" {
-  source        = "./module/server/main.tf"
+  source        = "./module/server"
   ami           = var.ami
   instance_type = var.instance_type
   cidr          = var.cidr
--- infra/module/server/main.tf
+++ infra/module/server/main.tf
@@ -1,45 +1,6 @@
 provider "aws" {
   region = "us-east-1"
 }
-####### VARIABLES  ###########
-
-variable "region_main" {
-  type        = string
-  # default = "us-east-1"
-}
-
-variable "cidr" {
-  type        = string
-  # default = "172.16.0.0/16"
-}
-
-variable "availability_zone_a" {
-  type        = string
-  # default = "us-east-1a" 
-}
-
-variable "availability_zone_b" {
-  type        = string
-  # default = "us-east-1b" 
-}
-
-variable "ami" {
-  type        = string
-  #  description = "ami-0a0e5d9c7acc336f1"
-}
-
-variable "instance_type" {
-  type        = string
-  # default = "t2.micro"
-}
-
-
-
-
-
-
-
-
 
 #creating vpc
 resource "aws_vpc" "myvpc" {

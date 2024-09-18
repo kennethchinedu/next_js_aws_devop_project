ff1401244f5cdbd67834a37d9b5a872f8dc48464
kennethchinedu <anamskenneth@gmail.com>
2024-09-18 02:23:36 +0100

    omo2

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-file-anams"
-    #dynamodb_table = "lock_files"
-    key = "global/mystatefile/terraform.tfstate"
-    region = "us-east-1"
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
-
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "terraform-state-file-anams"
-  versioning {
-    enabled = true
-  }
+module "app-deployment" {
+  source        = "./module/server"
+  # ami           = "ami-0a0e5d9c7acc336f1"
+  # instance_type = "t2.micro"
 }

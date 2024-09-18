e39634d39af10cf4d5b23905f6d5a6a76a92388a
kennethchinedu <anamskenneth@gmail.com>
2024-09-18 02:19:35 +0100

    omo

--- infra/main.tf
+++ infra/main.tf
@@ -1,21 +1,16 @@
-#We will be storing our statefile in aws s3 with dynamo db for statelock
 terraform {
   backend "s3" {
     bucket = "terraform-state-file-anams"
-    # dynamodb_table = "lock_files"
+    #dynamodb_table = "lock_files"
     key = "global/mystatefile/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
 
-module "app-deployment" {
-  source        = "./module/server"
-  availability_zone_a = var.availability_zone_a
-  availability_zone_b = var.availability_zone_b
-
-
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "terraform-state-file-anams"
+  versioning {
+    enabled = true
+  }
 }

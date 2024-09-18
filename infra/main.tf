30dc63b57b14c1bd831954822c6513d3f602c5d0
kennethchinedu <anamskenneth@gmail.com>
2024-09-18 02:10:45 +0100

    omo

--- infra/main.tf
+++ infra/main.tf
@@ -5,10 +5,5 @@ provider "aws" {
 
 module "app-deployment" {
   source        = "./module/server"
-  ami           = var.ami
-  instance_type = var.instance_type
-  cidr          = var.cidr
-  availability_zone_a = var.availability_zone_a
-  availability_zone_b = var.availability_zone_b
-  region_main   = var.region_main
+
 }

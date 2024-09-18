8ed9e1978138577e3d2b06dd241a512a41f47a45
kennethchinedu <anamskenneth@gmail.com>
2024-09-18 02:41:09 +0100

    omo35

--- infra/module/server/main.tf
+++ infra/module/server/main.tf
@@ -130,7 +130,7 @@ resource "aws_instance" "server1" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   ami                    = var.ami
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
+  #key_name               = aws_key_pair.deployer.key_name
   subnet_id             = aws_subnet.subnet1.id  
   user_data = base64encode(file("setup.sh"))
 

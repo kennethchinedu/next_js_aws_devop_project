55f7ba4fd002e7c89665ff9bd75d174204684352
kennethchinedu <anamskenneth@gmail.com>
2024-09-21 09:59:48 +0100

    dccd

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,12 @@ module "app-deployment" {
   ami                 = var.ami
   instance_type       = var.instance_type
 }
+
+
+output "ec2_public_ip" {
+  value = aws_instance.server1.public_ip
+}
+
+output "ec2_private_key" {
+  value = aws_instance.server1.private_ip
+}

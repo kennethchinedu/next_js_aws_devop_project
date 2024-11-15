5e3c38ede4c37af414412d693fd8c6fac2269dcb
kennethchinedu <anamskenneth@gmail.com>
2024-11-15 02:23:02 +0100

    network3

--- server_setup.sh
+++ server_setup.sh
@@ -106,7 +106,7 @@ echo "Backend Docker image pulled successfully."
 
 # Run the frontend container
 echo "Running the frontend container..."
-sudo docker run -d -p 5137:5137 --name "$FRONTEND_IMAGE" "$IMAGE_REG/$IMAGE_DIRECTORY/$FRONTEND_IMAGE:latest"
+sudo docker run -d -p 5173:5173 --name "$FRONTEND_IMAGE" "$IMAGE_REG/$IMAGE_DIRECTORY/$FRONTEND_IMAGE:latest"
 if [ $? -ne 0 ]; then
     echo "Failed to run frontend container. Exiting..."
     exit 1
